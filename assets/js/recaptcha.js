d63399de0379c61763105de7f16aa1f52653a018
Austin Hunt <austin.hunt.012@gmail.com>
2023-04-25 21:39:05 -0500

    recaptcha js created

---
+++ assets/js/recaptcha.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const recaptcha_secret_key = 'your_secret_key';
+  const recaptcha_response = document.querySelector('.g-recaptcha-response').value;
+  const remote_ip = event.target.dataset.remoteip;
+
+  const recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+  const recaptcha_params = new URLSearchParams();
+  recaptcha_params.append('secret', recaptcha_secret_key);
+  recaptcha_params.append('response', recaptcha_response);
+  recaptcha_params.append('remoteip', remote_ip);
+
+  const recaptcha_request = await fetch(recaptcha_url, {
+    method: 'POST',
+    body: recaptcha_params,
+  });
+  const recaptcha_result = await recaptcha_request.json();
+
+  if (recaptcha_result.success) {
+    // The reCAPTCHA was successfully verified
+    // Proceed with your form processing logic here
+    event.target.submit();
+  } else {
+    // The user is a bot, display an error message or take other appropriate action
+    alert('Please prove that you are not a robot');
+  }
+});
