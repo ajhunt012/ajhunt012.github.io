1e6cb6ae4e9cceb204b7c7cb5d9f2bf324e7a110
Austin Hunt <austin.hunt.012@gmail.com>
2023-05-03 22:48:44 -0500

    english changes

--- assets/js/recaptcha.js
+++ assets/js/recaptcha.js
@@ -23,10 +23,9 @@ form.addEventListener('submit', async (event) => {
     // Proceed with your form processing logic here
     event.target.submit();
     // need to keep the information hidden unitl this stage is completed including the second submit button ,and to clear this if it has been aknowledged. 
-// still having issues getting the information to populate.
 // TODO find whats affecting the non hidden side of the code. 
   } else {
     // The user is a bot, display an error message or take other appropriate action
-    alert('Please prove that you are not a robot');
+    alert('Confirm you are not a robot');
   }
 });
